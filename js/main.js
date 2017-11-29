96e9305832a2104098267c40bb68cff347a5dce2
Emily Buickerood <emilybuickerood.design@gmail.com>
2017-11-28 21:08:12 -0500

    finalized slideshow, need to fix css

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ $(document).ready(function(){
     console.log('slides: ', slides);
     var currentSlideIndex = 0; // start out at slide 0
     var slideCount = slides.length; // get how many items are in our slideshow-image
-    slides.eq(currentSlideIndex).show();
+    slides.eq(currentSlideIndex).fadeIn();
 
     // our function to run
     doSlides = function(direction){
       // hide current slides
-      slides.eq(currentSlideIndex).hide();
+      slides.eq(currentSlideIndex).fadeOut(500);
 
       if (direction === 'next'){
         // if statement to check if our current slide index = slideCount -1;
@@ -33,11 +33,17 @@ $(document).ready(function(){
       }
 
       console.log('currentSlideIndex: ', currentSlideIndex);
-      slides.eq(currentSlideIndex).show();
+      slides.eq(currentSlideIndex).fadeIn(500);
     };
 
 
+    $('#next-button').click(function() {
+      doSlides('next');
+    });
 
+    $('#prev-button').click(function() {
+      doSlides('prev');
+    });
 
 
 }); // closes doc.ready
